5171af39efc4cc6e503a6bada1802d3f712306d4
a.surovtsev <Exonanalytics2021_>
2025-04-28 16:53:24 +0300

    new JDBCTemplate service

---
+++ demoJDBCTemplate/src/test/java/ru/surovcev/project/demojdbctemplate/DemoJdbcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.surovcev.project.demojdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJdbcTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
